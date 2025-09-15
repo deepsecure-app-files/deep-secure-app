8fa70397726a108f8400c61cbec67a7d77e655f5
deepsecure-app-files <dg505434@gmail.com>
2025-09-15 18:23:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,24 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
-from os import environ
+from config import Config
+from models import db
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
 
-# CSRF Protection
-csrf = CSRFProtect(app)
+    # Import and register blueprints
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    # Setup database with app context
+    with app.app_context():
+        db.create_all()
 
-# App Configuration
-app.config['SECRET_KEY'] = environ.get('SECRET_KEY', 'default-key-for-development')
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'main.login'
-login_manager.login_message_category = 'info'
+app = create_app()
 
-from .models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-from .routes import main as main_blueprint
-app.register_blueprint(main_blueprint)
-
-# This part is for running the app locally. You don't need it on Render.
-# if __name__ == '__main__':
-#     with app.app_context():
-#         db.create_all()
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
