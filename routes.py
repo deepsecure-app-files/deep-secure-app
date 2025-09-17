57e28126914a13ec6df15926dea5bd00d9c25e59
deepsecure-app-files <dg505434@gmail.com>
2025-09-17 16:42:10 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -2,7 +2,6 @@ from flask import render_template, request, redirect, url_for, session, Blueprin
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 import secrets
-from models import db, User, Child, Geofence, LocationHistory
 from datetime import datetime
 import uuid
 
